fb4eddbf6374cd817ad3260f8801bc9b61c001e4
parthratra11 <parthratra11@gmail.com>
2025-05-24 02:48:14 +0530

    site logo updated v3

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export const metadata: Metadata = {
     siteName: "The Gifting Affair",
     images: [
       {
-        url: "/images/logo5.jpg",
+        url: "/images/logo6.png",
         width: 1200,
         height: 630,
         alt: "The Gifting Affair Logo",
